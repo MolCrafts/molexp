7244713a3d959fefb9b10bdc9ea54b32c005fbb0
Roy Kid <lijichen365@gmail.com>
2023-11-27 21:31:21 +0100

    start to use hamilton instead of metaflow

---
+++ src/molexp/experiments.py
@@ -0,0 +1,16 @@
+# author: Roy Kid
+# contact: lijichen365@126.com
+# date: 2023-11-27
+# version: 0.0.1
+
+import hamilton
+
+class Experiment:
+
+    def __init__(self):
+        pass
+
+class NNPTraning(Experiment):
+
+    def __init__(self):
+        pass
