96340d374c6b1e73dd14ef54a92dc30358560616
Roy Kid <lijichen365@gmail.com>
2024-01-13 21:55:29 +0100

    add project

--- src/molexp/project.py
+++ src/molexp/project.py
@@ -3,121 +3,112 @@
 # date: 2023-11-27
 # version: 0.0.1
 
-import shutil
-from typing import Literal
-import hamilton
-from hamilton import base, driver
-from hamilton.execution import executors
-from pathlib import Path
-from time import ctime
 import inspect
+import logging
+import shutil
 import textwrap
+import types
 import importlib
+
+from molexp.utils import WorkAt
+from .params import Params, Param
+from .experiment import Experiment, ExperimentGroup
+from pathlib import Path
+from hamilton import driver
+from hamilton import telemetry
+from hamilton.base import SimplePythonGraphAdapter
+from hamilton.experimental.h_cache import CachingGraphAdapter
+telemetry.disable_telemetry()
 
 __all__ = ["Project"]
 
-
-def convert_path_to_module(path):
-    module_path = Path(path).resolve()
-    module_name = module_path.relative_to(Path.cwd())
-    module = module_name.stem
-    package = module_name.parent.as_posix().replace("/", ".")
-    return "." + module, package
-
-
 class Project:
+
     def __init__(self, name, root: None | Path | str = None):
         self.name = name
+        self.logger = logging.getLogger(self.name)
         self.root = Path(root or Path.cwd())
-        self.init_workdir()
-
+        self.dir = self.root / self.name
         self.tasks = []
         self.dag_paths = []
+        if self.exists():
+            self.load()
+        else:
+            self.init()
+        self.meta = open(self.dir / 'meta.txt', 'w+')
 
-    def init_workdir(self):
-        logdir = self.root / f".log_{self.name}"
-        logdir.mkdir(exist_ok=True)
-        self.work_dir = logdir
-        # write metadata
-        with open(logdir / f"{self.name}.log", "w") as f:
-            f.write(f"# exp: {self.name}\n")
-            f.write(f"- time: {ctime()}\n")
-            f.write(' --- \n')
+    def __del__(self):
+        self.meta.close()
 
-    def init_experiment(self, paramSpec: ParamSpec):
-        for params in paramSpec.product():
-            exp = Experiment.From(params, self.work_dir)
-            self.experiments.append(exp)
+    def init(self):
+        self.dir.mkdir(parents=True)
+        self.logger.info(f"Project {self.name} is created at {self.dir}")
+        # TODO: add meta data and config
+
+    def load(self):
+        # TODO: check if dir is a valid project
+        self.logger.info(f"Project {self.name} is loaded from {self.dir}")
+
+    def exists(self):
+        return self.dir.exists()
+
+    def init_exp(self, params: Params):
+
+        expg = ExperimentGroup()
+        for param in params:
+            exp = Experiment(str(param), self.dir)
+            exp.init()
+            expg.append(exp)
+            self.meta.write(f"{exp.name}\n")
+
+        return expg
             
-    def select(self, paramSpace: ParamSpec):
+    def select(self, params: Params):
 
-        expgroup = ExperimentGroup()
+        expg = ExperimentGroup()
+        for param in params:
+            exp = Experiment(param, self.dir)
+            if exp.exists():
+                expg.append(exp)
+        return expg
+    
+    def select_all(self):
+        expg = ExperimentGroup()
+        self.meta.seek(0)
+        for line in self.meta.readlines():
+            exp = Experiment(str(Param.from_str(line)).rstrip('\n'), self.dir)
+            exp.load()
+            expg.append(exp)
+        return expg
+        
+        
+    def add_tasks(self, *modules):
+        for module in modules:
+            self.tasks.append(module)
+            if not Path(self.dir / module.__file__).exists():
+                shutil.copy(module.__file__, self.dir)
 
-        for params in paramSpace.product():
-            for exp in self.experiments:
-                if exp.params == params:
-                    expgroup.append(exp)
+    def add_task(self, module_name, *funcs):
+        with open(self.dir / f"{module_name}.py", "w") as f:
+            for func in funcs:
+                f.write(textwrap.dedent(inspect.getsource(func)))
+        module = importlib.import_module(f"{self.name}.{module_name}")
+        self.tasks.append(module)
+        return module
 
-        return expgroup            
+    def execute(self, input:dict, output:list, exp_group: ExperimentGroup | None = None, config={}):
 
-    def add_task(self, callable):
-        self.tasks.append(callable)
-
-    def add_tasks(self, module_path: Path | str):
-        path = Path(module_path)
-        shutil.copy(path, self.work_dir)
-        self.dag_paths.append(self.work_dir / path.name)
-
-    def export_dag(self, dag_name:str = "dag.py"):
-        src = ""
-        dag_path = self.work_dir / dag_name
-        with open(dag_path, "w") as f:
-            for node in self.tasks:
-                src += inspect.getsource(node)
-            wrapped_src = textwrap.dedent(
-                src,
+        exp_group = exp_group or self.select_all()
+        for exp in exp_group:
+            dr = (
+                driver.Builder()
+                .with_modules(*self.tasks)
+                .with_config(config)
+                .with_adapter(CachingGraphAdapter(str(exp.dir)))
+                .build()
             )
-            f.write(wrapped_src)
-        return dag_path
-
-    def execute(self, input:dict, output:list, config={}, adapter=None):
-        dag_module = []
-        if self.tasks:
-            dag_path = self.export_dag()
-            self.dag_paths.append(dag_path)
-
-        for path in self.dag_paths:
-            module, package = convert_path_to_module(path)
-            dag_module.append(importlib.import_module(module, package))
-        # adapter = base.SimplePythonGraphAdapter()
-
-        dr = (
-            driver.Builder()
-            .with_modules(*dag_module)
-            .enable_dynamic_execution(allow_project_mode=True)
-            .with_config({})
-            .with_local_executor(executors.SynchronousLocalTaskExecutor())
-            .with_remote_executor(executors.MultiProcessingExecutor(max_tasks=5))
-            .build()
-        )
-        out = dr.execute(output, inputs=input)
+            wa = WorkAt(exp.dir)
+            wa.cd_to()
+            out = dr.execute(output, inputs=input)
+            wa.cd_back()
         return out
-
-if __name__ == "__main__":
-    exp = Project("test", "test Project")
-
-    def foo(a: int, b: float) -> float:
-        """
-        add
-
-        Args:
-            a (int): input
-            b (float): input
-
-        Returns:
-            float: outputput
-        """
-        return a + b
-
-    exp.add_task(foo)
-    out = exp.execute(input={"a": 1, "b": 2.0}, output=["foo"])
