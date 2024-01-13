b359b8dfaeb42101906a2e8ce73b3bd623a0d056
Roy Kid <lijichen365@gmail.com>
2024-01-13 19:19:36 +0100

    add params and paramspace

---
+++ src/molexp/params.py
@@ -0,0 +1,34 @@
+# author: Roy Kid
+# contact: lijichen365@126.com
+# date: 2023-12-14
+# version: 0.0.1
+
+from typing import Sequence
+
+class Params(list):
+    
+    def __str__(self):
+        return f"Params({super().__str__()})"
+    
+    def __repr__(self):
+        return self.__str__()
+
+class ParamSpace(dict):
+
+    def __str__(self):
+        return f"ParamSpace({super().__str__()})"
+    
+    def __repr__(self):
+        return self.__str__()
+
+    def product(self, exclude: Sequence[str] = []):
+        """
+        exclude: list of keys to exclude
+        """
+        from itertools import product
+        keys = sorted([k for k in self.keys() if k not in exclude])
+        values = [self[k] for k in keys]
+        return Params(
+            dict(zip(keys, v))
+            for v in product(*values)
+        )
