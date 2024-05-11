ccbcbb771d8fd0ce0c41664695373464a40b7791
Roy Kid <lijichen365@gmail.com>
2024-05-11 12:10:45 +0200

    back to use hamilton.experimenttracker instead customized

--- src/molexp/param.py
+++ src/molexp/param.py
@@ -2,15 +2,19 @@ from itertools import product
 
 
 class Param(dict):
-    
+
     @property
     def name(self):
-        return '_'.join(f"{k}x{v}" for k, v in self.items())
+        raw = "_".join(f"{k}x{v}" for k, v in self.items())
+        return raw.replace(".", "-")
 
 class ParamList(list[Param]):
     pass
 
+
 class ParamSpace(dict):
-    
+
     def product(self):
-        return ParamList([Param(dict(zip(self.keys(), values))) for values in product(*self.values())])
+        return ParamList(
+            [Param(dict(zip(self.keys(), values))) for values in product(*self.values())]
+        )
--- tests/test_proj.py
+++ tests/test_proj.py
@@ -1,3 +1,5 @@
+from time import sleep
+import time
 import molexp as me
 from pathlib import Path
 
@@ -5,75 +7,83 @@ from hamilton.io.materialization import to
 from hamilton.function_modifiers import tag
 
 import numpy as np
+import shutil
 
-def calc_theta(freq:float, phase:float)->np.ndarray:
-    theta = np.linspace(0, 2*np.pi, 100)
-    return freq * theta + phase
 
-def calc_sine(calc_theta:np.ndarray)->np.ndarray:
-    sin = np.sin(calc_theta)
-    return sin
+def entry_point(freq: float, phase: float) -> np.ndarray:
+    print(f"Start time: {time.ctime()}")
+    return np.sin(freq * np.linspace(0, 2 * np.pi, 100) + phase)
 
-def load_sin(freq:float, phase:float)->np.ndarray:
-    sin = np.load('sine.pkl', allow_pickle=True)
-    return sin
+
+def short_task(entry_point: np.ndarray) -> np.ndarray:
+    return entry_point
+
+
+def long_task(short_task: np.ndarray) -> np.ndarray:
+    print("Sleeping for 3 seconds")
+    sleep(3)
+    return short_task
+
 
 class TestProject:
 
-    def test_init_exp(self):
+    def test_execute_exp(self):
 
-        if Path('test_exp').exists():
-            import shutil
-            shutil.rmtree('test_exp')
-        
         init_dir = Path.cwd()
 
-        params = me.ParamSpace({
-            'freq': [1., 2., 3.],
-            'phase': [0.],
-        })
+        params = me.ParamSpace(
+            {
+                "freq": [1.0, 2.0, 3.0],
+                "phase": [0.0],
+            }
+        )
 
         param_list = params.product()
 
         proj = me.Project("test_exp", Path.cwd())
         import test_proj
+
         materializers = [
             to.pickle(
-                id='m_calc_sine',
-                dependencies=['calc_sine'],
-                path='./sine.pkl'
-            )
+                id="pickle_short_task", dependencies=["short_task"], path="./pickle_short_task.pkl"
+            ),
+            to.pickle(
+                id="pickle_long_task", dependencies=["long_task"], path="./pickle_long_task.pkl"
+            ),
         ]
-        proj.execute(param_list, materializers, test_proj)
-        
+        proj.materialize(param_list, materializers, test_proj)
+
         end_dir = Path.cwd()
         assert init_dir == end_dir
 
-    def test_list_exp(self):
-        proj = me.Project("test_exp", Path.cwd())
-        exp_list = proj.list()
-        assert len(exp_list) == 3
+        # if Path('test_exp').exists():
+        #     shutil.rmtree('test_exp')
 
-    def test_map_reduce(self):
-        proj = me.Project("test_exp", Path.cwd())
-        exp_group = proj.list()
-        import test_proj
+    # def test_list_exp(self):
+    #     proj = me.Project("test_exp", Path.cwd())
+    #     exp_list = proj.list()
+    #     assert len(exp_list) == 3
 
-        mp = [
-            to.pickle(
-                id='m_load_sin',
-                dependencies=['load_sin'],
-                path='./sine1.pkl'
-            )
-        ]
-        def reducer(mapper:dict)->np.ndarray:
-            print(mapper)
-            results = {}
-            for exp_name, mat_result in mapper.items():
-                results[exp_name] = np.load(mat_result[0]['m_load_sin']['file_metadata']['path'], allow_pickle=True)
-            # calculate mean
-            return np.mean([result for result in results.values()], axis=0)
-        
-        results = exp_group.map_reduce(mp, reducer, test_proj)
-        print(results)
-        assert False
+    # def test_map_reduce(self):
+    #     proj = me.Project("test_exp", Path.cwd())
+    #     exp_group = proj.list()
+    #     import test_proj
+
+    #     mp = [
+    #         to.pickle(
+    #             id='m_load_sin',
+    #             dependencies=['load_sin'],
+    #             path='./sine1.pkl'
+    #         )
+    #     ]
+    #     def reducer(mapper:dict)->np.ndarray:
+    #         print(mapper)
+    #         results = {}
+    #         for exp_name, mat_result in mapper.items():
+    #             results[exp_name] = np.load(mat_result[0]['m_load_sin']['file_metadata']['path'], allow_pickle=True)
+    #         # calculate mean
+    #         return np.mean([result for result in results.values()], axis=0)
+
+    #     results = exp_group.map_reduce(mp, reducer, test_proj)
+    #     print(results)
+    #     assert False
